7921cab7807223c9f89988a997ce319ea30e40bf
Govind <imgovind95@gmail.com>
2026-01-29 16:05:08 +0530

    Project Updates

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -38,7 +38,7 @@ const Footer = () => {
       </div>
 
       {/* Copyright text */}
-      <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2025 © QuickBlog Govind - All Right Reserved.</p>
+      <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2026 © QuickBlog Govind - All Right Reserved.</p>
     </div>
   )
 }
